08e588d555630d17014ab648ca24f720538b7bba
chingrruu <ching-ru64@hotmail.com>
2015-08-04 01:05:36 +0800

    edited tutionclassdetails

--- dwa_chk1/TutorClass.cs
+++ dwa_chk1/TutorClass.cs
@@ -87,24 +87,24 @@ namespace dwa_chk1
             da.Fill(ds, "tuitionClasstutordetails");
             conn.Close();
 
-            if (ds.Tables["tutordetails"].Rows.Count > 0)
+            if (ds.Tables["tuitionClasstutordetails"].Rows.Count > 0)
             {
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["TName"].ToString()))
-                    name = Convert.ToString(ds.Tables["tutordetails"].Rows[0]["TName"]);
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["TSalutation"].ToString()))
-                    salutation = Convert.ToString(ds.Tables["tutordetails"].Rows[0]["TSalutation"]);
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["TTelno"].ToString()))
-                    telno = Convert.ToString(ds.Tables["tutordetails"].Rows[0]["TTelno"]);
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["TEmailAddr"].ToString()))
-                   email = Convert.ToString(ds.Tables["tutordetails"].Rows[0]["TEmailAddr"]);
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["TPassword"].ToString()))
-                    password = Convert.ToString(ds.Tables["tutordetails"].Rows[0]["TPassword"]);
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["FreeDayOfWeek"].ToString()))
-                    freedayofweek = Convert.ToString(ds.Tables["tutordetails"].Rows[0]["FreeDayOfWeek"]);
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["FreeTimeSlot"].ToString()))
-                    timeslot = Convert.ToString(ds.Tables["tutordetails"].Rows[0]["FreeTimeSlot"]);
-                if (!DBNull.Value.Equals(ds.Tables["tutordetails"].Rows[0]["TExperience"].ToString()))
-                    experience= Convert.ToInt32(ds.Tables["tutordetails"].Rows[0]["TExperience"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["TName"].ToString()))
+                    name = Convert.ToString(ds.Tables["tuitionClasstutordetails"].Rows[0]["TName"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["TSalutation"].ToString()))
+                    salutation = Convert.ToString(ds.Tables["tuitionClasstutordetails"].Rows[0]["TSalutation"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["TTelno"].ToString()))
+                    telno = Convert.ToString(ds.Tables["tuitionClasstutordetails"].Rows[0]["TTelno"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["TEmailAddr"].ToString()))
+                    email = Convert.ToString(ds.Tables["tuitionClasstutordetails"].Rows[0]["TEmailAddr"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["TPassword"].ToString()))
+                    password = Convert.ToString(ds.Tables["tuitionClasstutordetails"].Rows[0]["TPassword"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["FreeDayOfWeek"].ToString()))
+                    freedayofweek = Convert.ToString(ds.Tables["tuitionClasstutordetails"].Rows[0]["FreeDayOfWeek"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["FreeTimeSlot"].ToString()))
+                    timeslot = Convert.ToString(ds.Tables["tuitionClasstutordetails"].Rows[0]["FreeTimeSlot"]);
+                if (!DBNull.Value.Equals(ds.Tables["tuitionClasstutordetails"].Rows[0]["TExperience"].ToString()))
+                    experience = Convert.ToInt32(ds.Tables["tuitionClasstutordetails"].Rows[0]["TExperience"]);
                 
             }
             return 0;
